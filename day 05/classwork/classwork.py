a95440806697cddbf0e05ee2446914add3e04ffc
lukitooooooooo <lukaivanishvili105@gmail.com>
2025-03-06 01:27:08 +0400

    commit

---
+++ day 05/classwork/classwork.py
@@ -0,0 +1,7 @@
+name = input("please enter you name: ")
+last_name = input("please enter your last name: ")
+age = input("please enter your age: ")
+
+print("you name is"+ name)
+print("you last name is " + last_name)
+print("you are" + age)
---
+++ day 05/classwork/classwork3.py
@@ -0,0 +1,6 @@
+age = input("enter you age: ")
+
+if age >=:
+    print("you are allow to enter club ")
+else:
+    print("you are not alow to enter club")
