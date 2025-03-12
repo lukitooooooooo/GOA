a9b7329645c243201112e0d05ada9bd90f63763c
lukitooooooooo <lukaivanishvili105@gmail.com>
2025-03-12 17:32:00 +0400

    commit

---
+++ day 08/classwork/classwork.py
@@ -0,0 +1,3 @@
+number = int(input("enter number: "))
+
+print(number >= 5 and number <= 10)
---
+++ day 08/homework/homework2.py
@@ -0,0 +1,6 @@
+num1 = int(input('enetr number: '))
+
+if num1 >= 10 and num1 % 2 == 0:
+    print(True)
+else:
+    print(False)
