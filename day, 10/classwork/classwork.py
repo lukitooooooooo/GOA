7f4988b05a40b2b79755bcbd282f75a913826d2b
luka_ivanishvili <lukaivanishvili105@gmail.com>
2025-03-28 00:53:45 +0400

    Delete day, 10 directory

--- day, 10/classwork/classwork.py
+++
@@ -1,5 +0,0 @@
-num1 = int(input("Enter number: "))
-
-while num1 < 0:
-    print("Try again")
-    num1 = int(input("Enter number: "))  # Ask for a new input
--- day, 10/homework/homework4.py
+++
@@ -1,9 +0,0 @@
-password = "1234"
-
-u_password = input("Enter your password: ")
-
-while u_password != password:
-    print("Incorrect password. Try again.")
-    u_password = input("Enter your password: ")
-
-print("Your password is correct.")
