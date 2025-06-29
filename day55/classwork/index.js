68fb4e354cf427eb29ad71a252a53dd35c1280e8
lukitooooooooo <lukaivanishvili105@gmail.com>
2025-06-29 16:33:47 +0400

    second commit

---
+++ day55/classwork/index.js
@@ -0,0 +1,31 @@
+// classwork 1        start
+for (let i = 0; i < 5; i++) {
+    const randomNumber = Math.random();
+    console.log(randomNumber);
+}
+//                     end
+
+
+// classwork 2        start
+
+const name = "Luka";
+console.log(`Hello My name is "${name}"`);
+
+//                    end
+
+
+//  classwork 3     start
+
+// let და const ორივე გამოიყენება ცვლადების გამოსაცხადებლად
+// let - საშუალებას გაძლევს შექმნა ცვლადი, რომლის მნიშვნელობაც შემდგომში შეიძლება შეიცვალოს
+// const - ქმნის ცვლადს, რომლის მნიშვნელობის შეცვლა აღარ შეიძლება
+
+//                 end
+
+
+// classwork 4    start
+
+const fullName = "Luka";
+console.log(`Length of full name: ${fullName.length}`);
+
+//                end
---
+++ day55/homework/script.js
@@ -0,0 +1,52 @@
+console.log(5 + 3);
+console.log(10 + 20);
+console.log(1 + 1);
+console.log(-5 + 5);
+console.log(2 + 2.5);
+
+console.log(10 - 3);
+console.log(5 - 10);
+console.log(100 - 50);
+console.log(3.5 - 1.2);
+console.log(0 - 0);
+
+console.log(4 * 5);
+console.log(3 * 0);
+console.log(-2 * 3);
+console.log(1.5 * 2);
+console.log(10 * 10);
+
+console.log(10 / 2);
+console.log(9 / 3);
+console.log(5 / 2);
+console.log(-8 / 2);
+console.log(7 / 0);
+
+console.log(10 % 3);
+console.log(15 % 4);
+console.log(7 % 2);
+console.log(5 % 5);
+console.log(2 % 10);
+
+
+console.log(Math.random());
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+
+let name = "luka";
+let age = 14;
+
+// Template Literal (string literal)
+let text = `My name is ${name} and I am ${age} years old.`;
+
+console.log(text); // My name is luka and I am 14 years old.
+
+
+
+let name2 = "Nika";
+let age2 = 25;
+
+let text2 = "My name is " + name2 + " and I am " + age2 + " years old.";
+
+console.log(text2); // My name is Nika and I am 25 years old.
