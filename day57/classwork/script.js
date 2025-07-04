7d4b9df74d2c49d605d754c582d786ddce395462
lukitooooooooo <lukaivanishvili105@gmail.com>
2025-07-04 20:11:23 +0400

    second commit

---
+++ day57/classwork/script.js
@@ -0,0 +1,11 @@
+let car = "მერსედესი";
+
+if (car === "მერსედესი") {
+    console.log("კარგი არჩევანია!");
+} else if (car === "ტესლა") {
+    console.log("ცუდი არჩევანია!");
+} else if (car === "ოპელი") {
+    console.log("საშუალო არჩევანია!");
+} else {
+    console.log("ვერ გეტყვი");
+}
---
+++ day57/homework/script.js
@@ -0,0 +1,11 @@
+
+    const button = document.getElementById('myButton');
+    const heading = document.getElementById('myH1');
+
+
+button.onclick  = function(){
+    heading.textContent = 'hello world';
+}
+
+// DOM — ეს არის ვებ-გვერდის სტრუქტურა, რომელიც JavaScript-ს საშუალებას აძლევს, იმუშაოს HTML-ის ელემენტებთან.
+// getElementById არის JavaScript მეთოდი, რომელიც გვაძლევს წვდომას კონკრეტულ HTML ელემენტზე, თუ მას მინიჭებული აქვს ID.
